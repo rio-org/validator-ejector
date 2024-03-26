c64f7ae45a437bd2968ed1831dd9f8d7a6a75fdf
solimander <solimander-dev@protonmail.com>
2024-03-25 23:56:47 -0400

    Update docs for holesky

--- src/test/config.ts
+++ src/test/config.ts
@@ -4,7 +4,7 @@ import { makeConfig } from '../services/config/service.js'
 export const configBase = {
   EXECUTION_NODE: 'http://localhost:4445',
   CONSENSUS_NODE: 'http://localhost:4455',
-  OPERATOR_REGISTRY_ADDRESS: '0x8Abf8aB35915457219B801a774140f067DdB0705',
+  OPERATOR_REGISTRY_ADDRESS: '0x29c1e0cF40Dc96573076b2C8c271fe4AFfDfc631',
   OPERATOR_ID: '123',
   BLOCKS_PRELOAD: 10000,
   HTTP_PORT: 8080,
